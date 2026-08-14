31d912fabf67b81dc10945a27f7eaa2735da06d9
Kseniabars <kbursukova.02@gmail.com>
2026-08-14 15:12:03 +0300

    homework 2-2 db done

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -6,8 +6,6 @@ from .models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
     articles = Article.objects.prefetch_related('tags').order_by(ordering).all()
     context = {
