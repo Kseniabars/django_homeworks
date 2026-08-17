eecf8bba89b834f5c7e94e528e3dbac4d877100e
Kseniabars <kbursukova.02@gmail.com>
2026-08-17 18:49:26 +0300

    homework 2-2 db fixed

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -7,7 +7,7 @@ from .models import Article
 def articles_list(request):
     template = 'articles/news.html'
     ordering = '-published_at'
-    articles = Article.objects.prefetch_related('tags').order_by(ordering).all()
+    articles = Article.objects.prefetch_related('scopes').order_by(ordering).all()
     context = {
         'articles': articles
     }
